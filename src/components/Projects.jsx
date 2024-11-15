ed880db2caea96e1e45e6bf049d4fc36b7963ffb
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2024-11-15 19:11:08 +0530

    project section update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,57 +8,50 @@ function Projects() {
 
             <div className=' bg-secondary rounded-lg flex flex-col justify-center items-center gap-4 hover:bg-secondarylight'>
               <div className='text-2xl pt-3 text-primary'>UrStore</div>
-              <div><img src="/images/urstore.png" className='h-60' alt="" /></div>
+              <div><img src="/images/urstore.png" className='h-60 rounded-md' alt="" /></div>
               <div className='text-lg pl-3 pb-1 text-primary'>An ecommerece website for smooth transaction and common currency</div>
               <div className=' flex gap-4'>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Live</button>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Repo</button>
+                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'> <a href="https://urstorepbl.netlify.app/" target='blank'>Live</a></button>
+                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'> <a href="https://github.com/RaviPatel94/UrStore" target='blank'>Repo</a></button>
               </div>
             </div>
             <div className=' bg-secondary rounded-lg flex flex-col justify-center items-center gap-4 hover:bg-secondarylight'>
-              <div className='text-2xl pt-3 text-primary'>UrStore</div>
-              <div><img src="/images/urstore.png" className='h-60' alt="" /></div>
-              <div className='text-lg pl-3 pb-1 text-primary'>An ecommerece website for smooth transaction and common currency</div>
+              <div className='text-2xl pt-3 text-primary'>NewsVerifier</div>
+              <div><img src="/images/news.png" className='h-60 rounded-md' alt="" /></div>
+              <div className='text-lg pl-3 pb-1 text-primary'>An Ml model that tells if a new is true or false.</div>
               <div className=' flex gap-4'>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Live</button>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Repo</button>
+                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'> <a href="https://newsverifier.pythonanywhere.com/" target='blank'>Live</a></button>
+                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'> <a href="https://github.com/RaviPatel94/NewsVerifier" target='blank'>Repo</a></button>
               </div>
             </div>
             <div className=' bg-secondary rounded-lg flex flex-col justify-center items-center gap-4 hover:bg-secondarylight'>
-              <div className='text-2xl pt-3 text-primary'>UrStore</div>
-              <div><img src="/images/urstore.png" className='h-60' alt="" /></div>
-              <div className='text-lg pl-3 pb-1 text-primary'>An ecommerece website for smooth transaction and common currency</div>
+              <div className='text-2xl pt-3 text-primary'>WearLoop</div>
+              <div><img src="/images/wearloop.png" className='h-60 rounded-md' alt="" /></div>
+              <div className='text-lg pl-3 pb-1 text-primary'>a modern cloth renting website using HTML, CSS, and JavaScript</div>
               <div className=' flex gap-4'>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Live</button>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Repo</button>
+                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'> <a href="https://wearloop.netlify.app/" target='blank'>Live</a></button>
+                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'> <a href="https://github.com/RaviPatel94/wearloop" target='blank'>Repo</a></button>
               </div>
             </div>
             <div className=' bg-secondary rounded-lg flex flex-col justify-center items-center gap-4 hover:bg-secondarylight'>
-              <div className='text-2xl pt-3 text-primary'>UrStore</div>
-              <div><img src="/images/urstore.png" className='h-60' alt="" /></div>
-              <div className='text-lg pl-3 pb-1 text-primary'>An ecommerece website for smooth transaction and common currency</div>
+              <div className='text-2xl pt-3 text-primary'>WebGames</div>
+              <div><img src="/images/webgames.png" className='h-60 rounded-md' alt="" /></div>
+              <div className='text-lg pl-3 pb-1 text-primary'>A website featuring multiple mini-games, tackling various logical challenges, and improving problem-solving skills</div>
               <div className=' flex gap-4'>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Live</button>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Repo</button>
+                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'> <a href="https://webgames404.netlify.app/" target='blank'>Live</a></button>
+                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'> <a href="https://github.com/RaviPatel94/Web-games" target='blank'>Repo</a></button>
               </div>
             </div>
             <div className=' bg-secondary rounded-lg flex flex-col justify-center items-center gap-4 hover:bg-secondarylight'>
-              <div className='text-2xl pt-3 text-primary'>UrStore</div>
-              <div><img src="/images/urstore.png" className='h-60' alt="" /></div>
-              <div className='text-lg pl-3 pb-1 text-primary'>An ecommerece website for smooth transaction and common currency</div>
-              <div className=' flex gap-4'>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Live</button>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Repo</button>
+              <div className='text-2xl pt-3 text-primary'>Attendence Keeper</div>
+              <div><img src="/images/attendence.png" className='h-60 rounded-md' alt="" /></div>
+              <div className='text-lg pl-3 pb-1 text-primary'> A responsive web application, Attendance Keeper, that calculates and analyzes user attendance, providing insights on required or
+              optional lecture attendance based on current records.
               </div>
-            </div>
-            <div className=' bg-secondary rounded-lg flex flex-col justify-center items-center gap-4 hover:bg-secondarylight'>
-              <div className='text-2xl pt-3 text-primary'>UrStore</div>
-              <div><img src="/images/urstore.png" className='h-60' alt="" /></div>
-              <div className='text-lg pl-3 pb-1 text-primary'>An ecommerece website for smooth transaction and common currency</div>
               <div className=' flex gap-4'>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Live</button>
-                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'>Repo</button>
-              </div>
+                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'> <a href="https://attendence-keeper.netlify.app/" target='blank'>Live</a></button>
+                <button className='bg-primary text-secondary px-6 py-1.5 mb-3 text-lg rounded-md hover:rounded-2xl transition-all duration-300'> <a href="https://github.com/RaviPatel94/Attendence-Keeper" target='blank'>Repo</a></button>
+              </div>  
             </div>
 
         </div>
