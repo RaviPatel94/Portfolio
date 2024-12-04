4181aa7cc6b4e55c88683f24739b8673e130e4ec
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2024-12-04 14:01:23 +0530

    improve ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,10 +11,10 @@ function Footer() {
             <h1 className='text-2xl'>Mail me</h1>
             <form action="https://api.web3forms.com/submit" method="POST" className='flex flex-col justify-center items-center max-w-[250px] gap-4'>
             <input type="hidden" name="access_key" value="f452ef1d-ac63-4571-a65a-ba9e3eb41901"></input>
-            <input type="text" placeholder='Name' name="name" className=' w-64 h-9 rounded-md bg-secondarylight text-white outline-none px-3 placeholder-primary mt-3 ' required />
-            <input type="email" placeholder='Email' name="Email" className=' w-64 h-9 rounded-md bg-secondarylight text-white outline-none px-3 placeholder-primary' required />
-            <input type="text" placeholder='Query/ Feedback' name="message" className=' w-64 h-9 rounded-md bg-secondarylight text-white outline-none px-3 placeholder-primary mb-3' required/>
-            <input type="submit" className='text-lg border border-secondarylight px-3 py-[2px] rounded-xl w-max cursor-pointer hover:bg-secondarylight  hover:text-primary' />
+            <input type="text" placeholder='Name' name="name" className=' w-64 h-9 rounded-md bg-secondarylight text-primary outline-none px-3 placeholder-primary mt-3 ' required />
+            <input type="email" placeholder='Email' name="Email" className=' w-64 h-9 rounded-md bg-secondarylight text-primary outline-none px-3 placeholder-primary' required />
+            <input type="text" placeholder='Query/ Feedback' name="message" className=' w-64 h-9 rounded-md bg-secondarylight text-primary outline-none px-3 placeholder-primary mb-3' required/>
+            <input type="submit" className='text-lg border border-secondarylight px-3 py-[2px] rounded-md w-max cursor-pointer hover:bg-secondarylight  hover:text-primary hover:rounded-xl transition-all duration-300 ' />
             </form>
             </div>
         </div>
