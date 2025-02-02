44db0bde54dd0c956b550755688dac2154f26eca
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2025-02-03 01:31:08 +0530

    marquee effect

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,57 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 function Skills() {
   return (
     <div className='overflow-hidden w-screen'>
-      <h1 className='text-3xl text-center pb-1 sm:pl-16'>My skills</h1>
-      <div className='marquee-container flex items-center justify-center'>
-        <div className='marquee'>
+      <h1 className='text-3xl text-center pb-8 sm:pl-16'>My skills</h1>
+      <div className='marquee-container flex flex-col gap-2 items-center justify-center'>
+        <motion.div
+          className='marquee'
+          variants={{
+            loop: {
+              x: ["-40%", "0%"],
+              transition: {
+                duration: 7,
+                ease: 'linear',
+                repeat: Infinity,
+                repeatType: 'loop',
+              },
+            },
+          }}
+          animate="loop"
+        >
           <img src="https://skillicons.dev/icons?i=react,tailwind,js,html,css,bootstrap" alt="Skills" className="inline-block mr-2" />
+          {/* Duplicate the content to make it look infinite */}
+          <img src="https://skillicons.dev/icons?i=react,tailwind,js,html,css,bootstrap" alt="Skills" className="inline-block mr-2" />
+          <img src="https://skillicons.dev/icons?i=react,tailwind,js,html,css,bootstrap" alt="Skills" className="inline-block mr-2" />
+          <img src="https://skillicons.dev/icons?i=react,tailwind,js,html,css,bootstrap" alt="Skills" className="inline-block mr-2" />
+          <img src="https://skillicons.dev/icons?i=react,tailwind,js,html,css,bootstrap" alt="Skills" className="inline-block mr-2" />
+          <img src="https://skillicons.dev/icons?i=react,tailwind,js,html,css,bootstrap" alt="Skills" className="inline-block mr-2" />
+          
+        </motion.div>
+        <motion.div
+          className='marquee'
+          variants={{
+            loop: {
+              x: ["0%", "-40%"],
+              transition: {
+                duration: 7,
+                ease: 'linear',
+                repeat: Infinity,
+                repeatType: 'loop',
+              },
+            },
+          }}
+          animate="loop"
+        >
           <img src="https://skillicons.dev/icons?i=figma,git,firebase,vite,c,cpp,py" alt="Skills" className="inline-block mr-2" />
-        </div>
+          <img src="https://skillicons.dev/icons?i=figma,git,firebase,vite,c,cpp,py" alt="Skills" className="inline-block mr-2" />
+          <img src="https://skillicons.dev/icons?i=figma,git,firebase,vite,c,cpp,py" alt="Skills" className="inline-block mr-2" />
+          <img src="https://skillicons.dev/icons?i=figma,git,firebase,vite,c,cpp,py" alt="Skills" className="inline-block mr-2" />
+          <img src="https://skillicons.dev/icons?i=figma,git,firebase,vite,c,cpp,py" alt="Skills" className="inline-block mr-2" />
+          <img src="https://skillicons.dev/icons?i=figma,git,firebase,vite,c,cpp,py" alt="Skills" className="inline-block mr-2" />
+        </motion.div>
       </div>
     </div>
   );
