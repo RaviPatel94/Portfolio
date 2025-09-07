f7fca318591ed29f6b2158373beb5e3e629c8a16
RaviPatel94 <ravi942004@gmail.com>
2025-09-07 10:55:22 +0530

    updated resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,6 +20,7 @@ function Hero() {
     <div className='h-screen flex flex-col justify-center items-center ' id='hero'>
         <h2 className='font-normal text-24 leading-tight' >Hello, i am</h2>
         <h1 className=' font-normal text-7xl sm:text-8xl leading-[1] h-max' >Ravi Patel</h1>
+        
         <div className='w-[340px] flex justify-start items-start'>
         <h2  className='font-normal text-24 pt-1 flex gap-2 justify-start items-start '>
           a {designation}
